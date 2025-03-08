956971d0658c7f706c2f4a529b174cb02cfbedfa
Sachin Mundassery <sachin.mundassery>
2025-03-08 15:38:58 +0530

    added test cases

--- src/components/topics/Topics.test.jsx
+++ src/components/topics/Topics.test.jsx
@@ -1,68 +1,48 @@
 import {mockAxios, mockReactQuery, mockTolgee, mockUseAuth} from "../../test-utils/CommonMocks.js";
 import {QueryClient, QueryClientProvider} from "react-query";
 import * as reactQuery from "react-query";
-import { TolgeeProvider } from "@tolgee/react";
-import { fireEvent, render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {TolgeeProvider} from "@tolgee/react";
+import {fireEvent, render, screen} from "@testing-library/react";
+import {BrowserRouter as Router} from "react-router-dom";
 import "@testing-library/jest-dom";
 import Topics from "./Topics.jsx";
 import {vi} from "vitest";
 
 mockAxios();
-mockUseAuth()
-mockReactQuery()
+mockUseAuth();
+mockReactQuery();
 
 vi.mock("../../utils/Constants.js", () => ({
   getAlphabet: () => ["A", "B", "C", "T"],
-  LANGUAGE: "LANGUAGE"
+  LANGUAGE: "LANGUAGE",
 }));
 
 describe("Topics Component", () => {
-
   const queryClient = new QueryClient();
+
   const mockTopicsData = {
-    "topics": [
-      {
-        "id": "71adcc30-cfaa-4aee-9c1c-bda059a48e9e",
-        "title": "Topic 1"
-      },
-      {
-        "id": "5b8ed517-37c0-4daf-8ad9-a730c8d2f3ce",
-        "title": "Topic 2"
-      },
-      {
-        "id": "2fc5e913-3ed9-4e72-a630-b93d86a4ecfb",
-        "title": "Topic 3"
-      },
-      {
-        "id": "28d6e7bb-6bd3-41b0-9a72-474bfff146d3",
-        "title": "Topic 4",
-        "parent_id": {
-          "id": "28d6e7bb-6bd3-41b0-9a72-474bfff146d3",
-          "title": "Topic 4"
-        }
-      },
-      {
-        "id": "b87890b5-405a-41bf-8403-e06c7bad0ebb",
-        "title": "Topic 5"
-      }
-    ]
-  }
+    topics: [
+      {id: "67b9933f1ab71ec4d4a06ab6", title: "Happiness", has_child: true},
+      {id: "67b9996e1ab71ec4d4a06ab8", title: "Kindness", has_child: true},
+      {id: "67b99a361ab71ec4d4a06abb", title: "Patience", has_child: true},
+    ],
+  };
 
   beforeEach(() => {
-    vi.spyOn(reactQuery, 'useQuery').mockImplementation(() => ({
+    vi.restoreAllMocks(); // Ensure no mock contamination between tests
+    vi.spyOn(reactQuery, "useQuery").mockImplementation(() => ({
       data: mockTopicsData,
       isLoading: false,
     }));
-    vi.spyOn(Storage.prototype, 'getItem').mockReturnValue("bo-IN");
+    vi.spyOn(Storage.prototype, "getItem").mockReturnValue("bo-IN");
   });
 
   const setup = () => {
     render(
       <Router>
-        <QueryClientProvider client={ queryClient }>
-          <TolgeeProvider fallback={ "Loading tolgee..." } tolgee={ mockTolgee }>
-            <Topics />
+        <QueryClientProvider client={queryClient}>
+          <TolgeeProvider fallback={"Loading tolgee..."} tolgee={mockTolgee}>
+            <Topics/>
           </TolgeeProvider>
         </QueryClientProvider>
       </Router>
@@ -71,41 +51,65 @@ describe("Topics Component", () => {
 
   test("renders topics UI correctly", () => {
     setup();
-    expect(screen.getByText("Topic 1")).toBeInTheDocument();
-    expect(screen.getByText("Topic 2")).toBeInTheDocument();
-    expect(screen.getByText("Topic 3")).toBeInTheDocument();
+    expect(screen.getByText("Explore by Topic")).toBeInTheDocument()
+    expect(screen.getByText("Happiness")).toBeInTheDocument();
+    expect(screen.getByText("Kindness")).toBeInTheDocument();
+    expect(screen.getByText("Patience")).toBeInTheDocument();
+    expect(screen.getByText("Browse or search out complete list of topics.")).toBeInTheDocument()
   });
 
   test("displays loading state when data is being fetched", () => {
-    vi.spyOn(reactQuery, 'useQuery').mockImplementation(() => ({ 
-      data: null, 
-      isLoading: true 
+    vi.spyOn(reactQuery, "useQuery").mockImplementation(() => ({
+      data: null,
+      isLoading: true,
     }));
     setup();
-    expect(screen.getByText("Loading ...")).toBeInTheDocument();
+    expect(screen.getByText("Loading topics...")).toBeInTheDocument();
   });
 
   test("filters topics based on search input", () => {
     setup();
+    const nextButton = screen.getByText("Next")
+    expect(nextButton).toBeInTheDocument()
+    fireEvent.click(screen.getByText("All Topics A-Z"));
     const searchInput = screen.getByPlaceholderText("Search topics...");
-    fireEvent.change(searchInput, { target: { value: "Topic 1" } });
-    expect(screen.getByText("Topic 1")).toBeInTheDocument();
-    expect(screen.queryByText("Topic 2")).not.toBeInTheDocument();
+    expect(nextButton).not.toBeInTheDocument()
+    fireEvent.change(searchInput, {target: {value: "Patience"}});
+    expect(screen.getByText("Patience")).toBeInTheDocument();
   });
 
   test("filters topics by selected letter", () => {
     setup();
-    const letterButton = screen.getByText("T");
+    fireEvent.click(screen.getByText("All Topics A-Z"));
+    const letterButton = screen.getByText("H");
     fireEvent.click(letterButton);
-    expect(screen.getByText("Topic 1")).toBeInTheDocument();
+
+    expect(screen.getByText("Happiness")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("Happiness"))
+
   });
 
-  test("clears selected letter filter", () => {
+  test("clears selected letter filter", async () => {
     setup();
-    const letterButton = screen.getByText("T");
+    fireEvent.click(screen.getByText("All Topics A-Z"));
+    const letterButton = screen.getByText("H");
     fireEvent.click(letterButton);
-    fireEvent.click(screen.getByText("clear"));
-    expect(screen.getByText("Topic 1")).toBeInTheDocument();
-    expect(screen.getByText("Topic 5")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("topic.clear"));
+    expect(screen.getByPlaceholderText("Search topics...")).toBeInTheDocument()
   });
-})
+
+  test("back button", () => {
+    setup();
+    fireEvent.click(screen.getByText("All Topics A-Z"));
+    const backButton = screen.getByText("Back");
+    expect(backButton).toBeInTheDocument();
+    fireEvent.click(backButton)
+    expect(screen.getByText("All Topics A-Z")).toBeInTheDocument()
+  })
+
+  test("topics information card", () => {
+    setup();
+    expect(screen.getByText("Topic Information")).toBeInTheDocument()
+    expect(screen.getByText("Details about the selected topic will be displayed here.")).toBeInTheDocument()
+  })
+});
