2dd9b684e18e829ae8a8cb2a54ac77d047446e2c
Sachin Mundassery <sachin.mundassery>
2025-03-15 11:57:34 +0530

    added pagnation component, updated versions for api call

--- src/components/text-detail/versions/Versions.jsx
+++ src/components/text-detail/versions/Versions.jsx
@@ -1,15 +1,59 @@
 import "./Versions.scss"
 import {useTranslate} from "@tolgee/react";
+import {LANGUAGE, mapLanguageCode} from "../../../utils/Constants.js";
+import axiosInstance from "../../../config/axios-config.js";
+import {useQuery} from "react-query";
+import {useParams} from "react-router-dom";
+import React, {useMemo, useState} from "react";
+import PaginationComponent from "../../commons/pagination/PaginationComponent.jsx";
 
+
+export const fetchVersions = async (id, limit, skip) => {
+  const storedLanguage = localStorage.getItem(LANGUAGE);
+  const language = storedLanguage ? mapLanguageCode(storedLanguage) : "bo";
+
+  const {data} = await axiosInstance.get(`api/v1/texts/${id}/versions`, {
+    language,
+    limit,
+    skip
+  })
+  return data
+
+}
 const Versions = () =>{
+  const { id } = useParams();
   const { t } = useTranslate();
+  const [pagination, setPagination] = useState({ currentPage: 1, limit: 10 });
+  const skip = useMemo(() => (pagination.currentPage - 1) * pagination.limit, [pagination]);
 
+
+  // const { data: versionsData, isLoading } = useQuery(
+  //   ["texts", id, pagination.currentPage, pagination.limit],
+  //   () => fetchVersions(id, pagination.limit, skip),
+  //   {
+  //     refetchOnWindowFocus: false,
+  //     staleTime: 1000 * 60 * 20,
+  //     retry: 1
+  //   }
+  // );
   const languageMap = {
     "sa":"language.sanskrit",
     "bo":"language.tibetan",
     "en":"language.english"
   }
-  const data =  [
+
+  // if (isLoading) {
+  //   return <div className="notfound listtitle">Loading content...</div>;
+  // }
+
+  // if (!versionsData || !Array.isArray(versionsData.versions)) {
+  //   return <div className="notfound listtitle">
+  //     <div className="no-content">No content found</div>
+  //   </div>;
+  // }
+
+  const versionsData = {
+    data : [
       {
         "id": "uuid.v4",
         "title": "शबोधिचर्यावतार[sa]",
@@ -49,11 +93,26 @@ const Versions = () =>{
         "published_date": "2021-09-01T00:00:00.000Z",
         "published_by": "buddhist_tab"
       }
-    ]
+    ],
+    total: 10,
+    skip: 0,
+    limit: 10
+  }
+  const totalVersions = versionsData?.total || 0;
+  const totalPages = Math.ceil(totalVersions / pagination.limit);
+
+  const handlePageChange = (pageNumber) => {
+    setPagination(prev => ({ ...prev, currentPage: pageNumber }));
+  };
+
+  const handleLimitChange = (e) => {
+    setPagination({ currentPage: 1, limit: Number(e.target.value) });
+  };
+
   return (
     <div className="versions-container">
       {
-        data.map((version,index) => <>
+        versionsData?.data.map((version,index) => <>
           <div  key={index} className="version">
             <div>
               <div className="version-title listtitle">
@@ -71,6 +130,14 @@ const Versions = () =>{
           <hr/>
         </>)
       }
+
+      {versionsData.data.length > 0 &&
+        <PaginationComponent
+          pagination={pagination}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+          setPagination={setPagination}
+        />}
     </div>
   )
 }
--- src/components/topics/Topics.jsx
+++ src/components/topics/Topics.jsx
@@ -1,13 +1,14 @@
 import {LANGUAGE, mapLanguageCode} from "../../utils/Constants.js";
 import axiosInstance from "../../config/axios-config.js";
 import {useQuery} from "react-query";
-import {useEffect, useMemo, useState} from "react";
+import {useMemo, useState} from "react";
 import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
 import {Button, Card, Col, Container, Form, Pagination, Row} from "react-bootstrap";
 import "./Topics.scss"
 import React from "react";
 import {useTranslate} from "@tolgee/react";
 import {useDebounce} from "use-debounce";
+import PaginationComponent from "../commons/pagination/PaginationComponent.jsx";
 
 export const fetchTopics = async (parentId, searchFilter, limit, skip, hierarchy) => {
   const storedLanguage = localStorage.getItem(LANGUAGE);
@@ -101,7 +102,13 @@ const Topics = () => {
               </Col>
             )}
           </Row>
-          {filteredTopics.length > 0 && <Row>{renderPagination()}</Row>}
+          {filteredTopics.length > 0 && <Row>
+            <PaginationComponent
+            pagination={pagination}
+            totalPages={totalPages}
+            handlePageChange={handlePageChange}
+            setPagination={setPagination}
+          /></Row>}
         </>
       )
     );
@@ -158,33 +165,6 @@ const Topics = () => {
     );
   };
 
-  const renderPagination = () => {
-    return (
-      <div className="pagination-container">
-        <Pagination>
-          <Pagination.Prev onClick={() => handlePageChange(pagination.currentPage - 1)} disabled={pagination.currentPage === 1} />
-          {[...Array(totalPages).keys()].map(number => (
-            <Pagination.Item
-              key={number + 1}
-              active={number + 1 === pagination.currentPage}
-              onClick={() => handlePageChange(number + 1)}
-            >
-              {number + 1}
-            </Pagination.Item>
-          ))}
-          <Pagination.Next onClick={() => handlePageChange(pagination.currentPage + 1)} disabled={pagination.currentPage === totalPages} />
-        </Pagination>
-        <Form.Group controlId="limitSelect" className="mb-3">
-          <Form.Select value={pagination.limit} onChange={(e) => setPagination({currentPage: 1, limit: Number(e.target.value)})}>
-            <option value="10">10</option>
-            <option value="12">12</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-          </Form.Select>
-        </Form.Group>
-      </div>
-    );
-  };
 
   return (
     <Container fluid className="topics-container">
