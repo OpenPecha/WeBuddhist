cf23d920bc53c98f90336a833d259459b14bb462
Tech-lo <taylorhent@gmail.com>
2025-05-21 15:07:13 +0545

    add pagination layout in sheets tab

--- src/components/search/sheets/Sheets.jsx
+++ src/components/search/sheets/Sheets.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslate } from '@tolgee/react';
 import './Sheets.scss';
+import { Button } from 'react-bootstrap';
 
 const Sheets = () => {
   const { t } = useTranslate();
@@ -10,6 +11,21 @@ const Sheets = () => {
       <p className="results-message">
         {t('search.zero_result', 'No results to display.')}
       </p>
+      <div className="pagination-container">
+        <Button 
+          variant="outline-secondary" 
+          className="pagination-btn prev-btn"
+          disabled={true} 
+        >
+          &laquo;
+        </Button>
+        <Button 
+          variant="outline-secondary" 
+          className="pagination-btn next-btn"
+        >
+          &raquo;
+        </Button>
+      </div>
     </div>
   );
 };
