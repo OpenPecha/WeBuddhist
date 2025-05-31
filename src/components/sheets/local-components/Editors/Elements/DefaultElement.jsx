2a475eee80c0298e3611c91eea88f617785f9c94
TenzDelek <tibetdelek@gmail.com>
2025-05-31 09:39:02 +0530

    new line visual

--- src/components/sheets/local-components/Editors/Elements/DefaultElement.jsx
+++ src/components/sheets/local-components/Editors/Elements/DefaultElement.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import './DefaultElement.scss';
 const DefaultElement = props => {
     const { element, attributes, children } = props;
     const style = {
       ...(element.align ? { textAlign: element.align } : {}),
-      whiteSpace: 'pre-wrap' // This preserves line breaks and whitespace
+      whiteSpace: 'pre-wrap' 
     };
-    return <p style={style} {...attributes}>{children}</p>
+    
+    return (
+      <p style={style} {...attributes} className="paragraph-with-indicator">
+        <span className="newline-indicator"/>
+        {children}
+      </p>
+    );
   }
 
-  export default DefaultElement
+export default DefaultElement
