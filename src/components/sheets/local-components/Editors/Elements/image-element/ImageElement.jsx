308272ad6762b8685b3e19111db03b0e278e347d
TenzDelek <tibetdelek@gmail.com>
2025-06-14 10:44:42 +0530

    update not found image

--- src/components/sheets/local-components/Editors/Elements/image-element/ImageElement.jsx
+++ src/components/sheets/local-components/Editors/Elements/image-element/ImageElement.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageElement = props => {
     const { attributes, children, element } = props;
     const { src, url } = element;
-    const noImageUrl = "https://icrier.org/wp-content/uploads/2022/12/media-Event-Image-Not-Found.jpg"; 
+    const noImageUrl = "https://app-pecha-backend.s3.amazonaws.com/images/sheet_images/2fc1ea97-6e19-45f5-a3f4-be6e32b4fe4d/notreal.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5FCD6NLK3T7GINFJ%2F20250614%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20250614T051314Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=684113de3fba82a80f9de6f45a3ea9b7bc48d489f95ac1427b792f83ce4bb820"; 
     
     if (!src) {
         return (
@@ -23,7 +23,6 @@ const ImageElement = props => {
                 <img
                     className="sheet-image"
                     src={src}
-                    alt={element.alt || 'Sheet image'}
                     onError={(e) => {
                         e.target.onerror = null;
                         e.target.src = noImageUrl;
