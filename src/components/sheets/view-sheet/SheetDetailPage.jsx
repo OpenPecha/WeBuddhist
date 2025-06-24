3a8138e08a2a688a45925994cea348edc694878e
TenzDelek <tibetdelek@gmail.com>
2025-06-24 11:01:46 +0530

    viewtoolbar and some other changes

--- src/components/sheets/view-sheet/SheetDetailPage.jsx
+++ src/components/sheets/view-sheet/SheetDetailPage.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import { getLanguageClass } from '../../../utils/Constants';
 import pechaIcon from '../../../assets/icons/pecha_icon.png';
 import './SheetDetailPage.scss';
-
+import YouTube from 'react-youtube';
+import { FiEdit ,FiTrash,FiHeart,FiEye} from "react-icons/fi";
 //TODO: to be remove when api is ready
 const sheetData = {
   sheet_title: "The great Dhrama of Tibet ",
@@ -34,12 +35,21 @@ const sheetData = {
             segment_id: "125",
             segment_number: "1.3",
             type: "image",
-            media_url: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+            content: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+          },
+          {
+            segment_id: "126",
+            segment_number: "1.4",
+            type: "audio",
+            content: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" //TODO: do changes such that spotify and soundcloud ui can be used
+          },
+          {
+            segment_id: "127",
+            segment_number: "1.5",
+            type: "video",
+            content: "5noIKN8t69U"
           }
         ],
-        created: "2024-03-20",
-        updated: "2024-03-21",
-        published: "2024-03-12"
       },
     ]
   },
@@ -57,7 +67,6 @@ const SheetDetailPage = () => {
       case 'source':
         return (
           <div className="segment segment-source" key={segment.segment_id}>
-            <div className="segment-number">{segment.segment_number}</div>
             <div className="source-content">
               <img src={pechaIcon} className='pecha-icon' alt="source icon" />
               <div className={getLanguageClass(segment.language || 'en')}>
@@ -70,17 +79,21 @@ const SheetDetailPage = () => {
       case 'text':
         return (
           <div className="segment segment-text" key={segment.segment_id}>
-            <div className="segment-number">{segment.segment_number}</div>
             <p className="text-content">{segment.content}</p>  {/* TODO: just create it as dangerous inner html  */}
           </div>
         );
-      case 'image': //TODO : case for audio and video
+      case 'image': //TODO : case for audio
         return (
           <div className="segment segment-image" key={segment.segment_id}>
-            <div className="segment-number">{segment.segment_number}</div>
             <figure>
-              <img src={segment.media_url} alt="Sheet content" />
+              <img src={segment.content} alt="Sheet content" />
             </figure>
+          </div>
+        );
+      case 'video':
+        return (
+          <div className="segment segment-video" key={segment.segment_id}>
+            <YouTube videoId={segment.content} />
           </div>
         );
       default:
@@ -90,26 +103,50 @@ const SheetDetailPage = () => {
 
   const renderHeader = () => {
     return (
-      <header className="sheet-detail-page-header">
          <header className="sheet-detail-page-header">
           <h1>{sheetData.sheet_title}</h1>
-          <div className="metadata">
-            <time>Published: {sheetData.content.sections[0].published}</time>
-            <time>Last updated: {sheetData.content.sections[0].updated}</time>
-          </div>
         </header>
-      </header>
     );
   };
 
+  const renderViewToolbar=()=>{
+    return(
+      <div className="view-toolbar">
+        <div className="view-toolbar-item">
+          <div className="view-toolbar-item-like">
+          <FiHeart />
+          <p>120</p>
+          </div>
+          <div className="view-toolbar-item-eye">
+          <FiEye />
+          <p>20</p>
+          </div>
+        </div>
+        <div className="view-toolbar-item">
+            <FiEdit />
+            <FiTrash />
+        </div>
+      </div>
+    )
+  }
+
+  const renderUserInfo=()=>{
+    return(
+      <div className="user-info">
+        <img src="https://avatars.githubusercontent.com/u/122612557?v=4" alt="user" className='user-info-avatar' />
+        <div className="user-info-text">
+          <p>Tenzin Delek</p>
+          <p>@tenzin_delek.3248</p>
+        </div>
+      </div>
+    )
+  }
   return (
     <main className="sheet-detail-container">
       <article className="sheet-detail-page">
         {renderHeader()}
-        <div>
-          {/* use format as as how i did for renderheader */}
-          {/* TODO: here create the ui same as the medium (the view,like,edit,delete) import icon from react-icons */}
-        </div>
+        {renderUserInfo()}
+        {renderViewToolbar()}
         <div className="sheet-content">
           {sheetData.content.sections.map((section) => (
             <section key={section.id} className="sheet-section">
