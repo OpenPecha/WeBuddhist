c6084ffb7be121e751999d59f0df3f0cc9251caf
Lungsangg <lungsang@esukhia.org>
2025-07-26 15:50:24 +0530

    short_url_from_backend_integrated

--- src/components/sheets/local-components/sheet-share/sheetShare.jsx
+++ src/components/sheets/local-components/sheet-share/sheetShare.jsx
@@ -1,12 +1,54 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { FiShare2, FiCopy } from 'react-icons/fi';
 import { FaFacebook, FaTwitter } from 'react-icons/fa';
+import { useQuery } from 'react-query';
+import axiosInstance from '../../../../config/axios-config.js';
 import './SheetShare.css';
 
-const SheetShare = ({ url = window.location.href }) => {
+// ----------------------------- helpers ---------------------------------------
+
+const extractTextIdFromUrl = (url) => {
+  try {
+    const urlPath = new URL(url).pathname;
+    const lastPathPart = urlPath.split('/').pop();
+    const textId = lastPathPart.split('_').pop();
+    return textId;
+  } catch (error) {
+    console.error('Error extracting text_id from URL:', error);
+    return null;
+  }
+};
+
+// ----------------------------- api calls ------------------------------------
+
+export const fetchShortUrl = async (url, textId, language = 'bo') => {
+  const { data } = await axiosInstance.post('/api/v1/share', { 
+    text_id: textId,
+    language: language,
+    url,
+  });
+  return data;
+};
+
+const SheetShare = ({ url = window.location.href, language = 'bo' }) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
   const [copySuccess, setCopySuccess] = useState(false);
+  
+  const textId = extractTextIdFromUrl(url);
+
+  const { data: shorturlData, isLoading } = useQuery(
+    ["shortUrl", url, textId, isOpen],
+    () => fetchShortUrl(url, textId, language),
+    {
+      refetchOnWindowFocus: false,
+      enabled: isOpen && Boolean(textId),
+    }
+  );
+
+  const getShareableUrl = () => {
+    return shorturlData?.shortUrl || url;
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -30,12 +72,14 @@ const SheetShare = ({ url = window.location.href }) => {
     }
   }, [copySuccess]);
 
+  // ----------------------------- event handlers -------------------------------
+
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
   const handleCopyLink = () => {
-    navigator.clipboard.writeText(url)
+    navigator.clipboard.writeText(getShareableUrl())
       .then(() => {
         setCopySuccess(true);
         setTimeout(() => setIsOpen(false), 1000);
@@ -50,10 +94,10 @@ const SheetShare = ({ url = window.location.href }) => {
     
     switch (platform) {
       case 'twitter':
-        shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
+        shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(getShareableUrl())}`;
         break;
       case 'facebook':
-        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(getShareableUrl())}`;
         break;
       default:
         return;
@@ -62,6 +106,8 @@ const SheetShare = ({ url = window.location.href }) => {
     window.open(shareUrl, '_blank', 'width=600,height=400');
     setIsOpen(false);
   };
+
+  // ----------------------------- renderers -------------------------------------
 
   return (
     <div className="sheet-share-container" ref={dropdownRef}>
@@ -78,14 +124,16 @@ const SheetShare = ({ url = window.location.href }) => {
           <button 
             className="share-option" 
             onClick={handleCopyLink}
+            disabled={isLoading && !textId}
           >
             <FiCopy className="share-icon" />
-            <span>Copy link</span>
+            <span>{isLoading ? "Loading..." : textId ? "Copy link" : "Invalid URL"}</span>
           </button>
           
           <button 
             className="share-option" 
             onClick={() => shareOnPlatform('twitter')}
+            disabled={isLoading || !textId}
           >
             <FaTwitter className="share-icon" />
             <span>Share on X</span>
@@ -94,6 +142,7 @@ const SheetShare = ({ url = window.location.href }) => {
           <button 
             className="share-option" 
             onClick={() => shareOnPlatform('facebook')}
+            disabled={isLoading || !textId}
           >
             <FaFacebook className="share-icon" />
             <span>Share on Facebook</span>
