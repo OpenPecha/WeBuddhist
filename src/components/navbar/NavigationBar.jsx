1d49f4ad040ddcba5835eb8366a82a1034107d15
TenzDelek <tibetdelek@gmail.com>
2025-08-06 14:11:58 +0530

    change icon

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { FaGlobe, FaSearch } from "react-icons/fa";
-import { LuEyeClosed, LuEye} from "react-icons/lu";
+import { RxHamburgerMenu , RxCross1 } from "react-icons/rx";
 import { useAuth } from "../../config/AuthContext.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
 import { ACCESS_TOKEN, LANGUAGE, LOGGED_IN_VIA, REFRESH_TOKEN } from "../../utils/constants.js";
@@ -193,7 +193,7 @@ const renderAuthButtons = () => {
                 className="mobile-menu-trigger"
                 onClick={handleMobileMenuToggle}
               >
-                {!isMobileMenuOpen ? <LuEyeClosed /> : <LuEye />}
+                {!isMobileMenuOpen ? <RxHamburgerMenu /> : <RxCross1 />}
               </button>
           </div>
       </div>
