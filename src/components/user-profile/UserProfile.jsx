eede7f0250536a5dc4e02d01edb4a97732e2e207
Lungsangg <lungsang@esukhia.org>
2025-09-26 16:09:31 +0530

    social_with_url_only_visible

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -163,9 +163,11 @@ const UserProfile = () => {
       email: { icon: BsEnvelope, color: "#FF0000" },
     };
 
+    const profilesWithUrls = socialProfiles.filter(profile => profile.url && profile.url.trim() !== '');
+
     return (
       <div className="social-links">
-        {socialProfiles.map((profile) => {
+        {profilesWithUrls.map((profile) => {
           const { icon: Icon, color } = socialIcons[profile.account] || {};
           return Icon ? (
             <a
@@ -187,16 +189,19 @@ const UserProfile = () => {
     );
   };
 
-  const renderProfileLeftSection = () => (
-    <div className="profile-left">
-      {renderBasicInfo()}
-      {renderProfileDetails()}
-      {renderActionButtons()}
-      {renderFollowersInfo()}
-      {userInfo?.social_profiles?.length > 0 &&
-        renderSocialLinks(userInfo?.social_profiles)}
-    </div>
-  );
+  const renderProfileLeftSection = () => {
+    const hasValidSocialProfiles = userInfo?.social_profiles?.some(profile => profile.url && profile.url.trim() !== '');
+    
+    return (
+      <div className="profile-left">
+        {renderBasicInfo()}
+        {renderProfileDetails()}
+        {renderActionButtons()}
+        {renderFollowersInfo()}
+        {hasValidSocialProfiles && renderSocialLinks(userInfo?.social_profiles)}
+      </div>
+    );
+  };
 
   const renderProfileImage = () => (
     <div className="profile-image-container">
