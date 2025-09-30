b32e4ed6e5168382f3c006b207ae880e6b0fd6df
TenzDelek <tibetdelek@gmail.com>
2025-09-30 12:21:38 +0530

    test

--- src/components/community/CommunityPage.test.jsx
+++ src/components/community/CommunityPage.test.jsx
@@ -86,7 +86,7 @@ describe("CommunityPage Component", () => {
     // expect(screen.getByText(/Organizations, communities and individuals/)).toBeInTheDocument();
     // expect(screen.getByText("Explore Collections")).toBeInTheDocument();
     expect(screen.getByText(/Combine sources from our library/)).toBeInTheDocument();
-    expect(screen.getByText("side_nav.make_a_story")).toBeInTheDocument();
+    expect(screen.getByText("Make a Sheet")).toBeInTheDocument();
   });
 
   test("renders sheet items correctly", () => {
@@ -155,7 +155,7 @@ describe("CommunityPage Component", () => {
 
   test("handles Make a Sheet button interaction", () => {
     setup();  
-    const makeSheetButton = screen.getByText("side_nav.make_a_story");
+    const makeSheetButton = screen.getByText("Make a Sheet");
     expect(makeSheetButton).toBeInTheDocument();
     expect(makeSheetButton).not.toBeDisabled();
     fireEvent.click(makeSheetButton);
