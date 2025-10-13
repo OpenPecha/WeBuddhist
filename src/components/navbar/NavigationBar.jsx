5cca88a734e74eb6bce480ab15b59e9f2bbd9c29
TenzDelek <tibetdelek@gmail.com>
2025-10-13 14:58:53 +0530

    no text handle

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -14,6 +14,12 @@ export const invalidateQueries = async (queryClient) => {
     const queriesToInvalidate = ["texts", "topics","sheets","sidePanel","works","texts-versions","texts-content","sheets-user-profile","table-of-contents","collections","sub-collections","versions"];
     await Promise.all(queriesToInvalidate.map(query => queryClient.invalidateQueries(query)));
   };
+ export const changeLanguage = async (lng,queryClient,tolgee) => {
+    await tolgee.changeLanguage(lng);
+    localStorage.setItem(LANGUAGE, lng);
+    setFontVariables(lng);
+    await invalidateQueries(queryClient)
+  };
 const Navigation = () => {
     const navigate = useNavigate();
     const { t } = useTranslate();
@@ -25,12 +31,7 @@ const Navigation = () => {
     const [isLangDropdownOpen, setIsLangDropdownOpen] = useState(false);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
    
-     const changeLanguage = async (lng) => {
-       await tolgee.changeLanguage(lng);
-       localStorage.setItem(LANGUAGE, lng);
-       setFontVariables(lng);
-       await invalidateQueries(queryClient)
-     };
+
      function handleLogout(e) {
         e.preventDefault()
         localStorage.removeItem(LOGGED_IN_VIA);
@@ -53,8 +54,8 @@ const Navigation = () => {
       handleMobileMenuToggle();
     };
 
-    const handleLangSelect = (lng) => {
-      changeLanguage(lng);
+    const handleLangSelect = (lng,queryClient) => {
+      changeLanguage(lng,queryClient,tolgee);
       setIsLangDropdownOpen(false);
     };
 
@@ -131,7 +132,7 @@ const renderAuthButtons = () => {
     );
   };
   const globalLangSelect=(lang)=>{
-    handleLangSelect(lang);
+    handleLangSelect(lang,queryClient,tolgee);
     handleMobileMenuToggle();
   }
   const renderLanguageOptions=()=>{
