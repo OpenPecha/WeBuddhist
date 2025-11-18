e6a8f36afccb4efbdb46a03ad53c6640ad302c0f
Tech-lo <taylorhent@gmail.com>
2025-11-18 12:24:16 +0530

    rest

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
       <div className="footer-column">
         <h3 className="footer-column-title">{t("footer.developers")}</h3>
         <ul className="footer-links">
-          <li><a href="https://github.com/OpenPecha" target="_blank" rel="noopener noreferrer">{t('fork_us_on_github')}</a></li>
+          <li><a href="https://github.com/OpenPecha" target="_blank" rel="noopener noreferrer">{t('footer.fork_us_on_github')}</a></li>
         </ul>
       </div>
     );
@@ -44,12 +44,12 @@ const Footer = () => {
       <div className="footer-connect">
         <h3 className="footer-column-title">CONNECT</h3>
         <div className="social-links">
-          <a href="https://www.instagram.com/dharmaduta.in/" target="_blank" rel="noopener noreferrer">{t('Instagram')}</a>
-          <a href="https://www.facebook.com/profile.php?id=61578322432088" target="_blank" rel="noopener noreferrer">{t('Facebook')}</a>
-          <a href="https://www.youtube.com/@DharmadutaServicesLLP" target="_blank" rel="noopener noreferrer">{t('Youtube')}</a>
-          <a href="https://www.linkedin.com/company/dharmaduta-services-llp/" target="_blank" rel="noopener noreferrer">{t('Linkedin')}</a>
-          <a href="https://discord.com/invite/7GFpPFSTeA" target="_blank" rel="noopener noreferrer">{t('Discord')}</a>
-          <a href="mailto:contact@dharmaduta.in" target="_blank" rel="noopener noreferrer">{t('Email')}</a>
+          <a href="https://www.instagram.com/dharmaduta.in/" target="_blank" rel="noopener noreferrer">{t('footer.instagram')}</a>
+          <a href="https://www.facebook.com/profile.php?id=61578322432088" target="_blank" rel="noopener noreferrer">{t('footer.facebook')}</a>
+          <a href="https://www.youtube.com/@DharmadutaServicesLLP" target="_blank" rel="noopener noreferrer">{t('footer.youtube')}</a>
+          <a href="https://www.linkedin.com/company/dharmaduta-services-llp/" target="_blank" rel="noopener noreferrer">{t('footer.linkedin')}</a>
+          <a href="https://discord.com/invite/7GFpPFSTeA" target="_blank" rel="noopener noreferrer">{t('footer.discord')}</a>
+          <a href="mailto:contact@dharmaduta.in" target="_blank" rel="noopener noreferrer">{t('footer.email')}</a>
         </div>
       </div>
     );
