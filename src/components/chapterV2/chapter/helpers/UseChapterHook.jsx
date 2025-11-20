a6d87e78c5cdfbeee9b8b4c7e88407b918c248a2
TenzDelek <tibetdelek@gmail.com>
2025-11-20 23:45:18 +0530

    increase width

--- src/components/chapterV2/chapter/helpers/UseChapterHook.jsx
+++ src/components/chapterV2/chapter/helpers/UseChapterHook.jsx
@@ -179,7 +179,7 @@ const UseChapterHook = (props) => {
     const languageClass = getLanguageClass(language);
     return (
       <div
-        className={`contents-container ${isProse ? "prose-layout" : ""}`}
+        className={`contents-container border ${isProse ? "prose-layout" : ""}`}
         key={section.title || "root"}
         ref={(sectionRef) => {
           sectionRef &&
