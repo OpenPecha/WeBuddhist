fd7c3c316f79332fda10ea3dca42f7d49d75efbd
Tech-lo <taylorhent@gmail.com>
2025-12-08 12:21:56 +0530

    pass classname prop

--- src/components/sheets/local-components/Editors/Elements/style-elements/ListItem.jsx
+++ src/components/sheets/local-components/Editors/Elements/style-elements/ListItem.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from "prop-types";
+import { getLanguageClass } from "../../../../../../utils/helperFunctions.jsx";
+
 const ListItem = (props) => {
   const { attributes, children, element } = props
   const style = element?.align ? { textAlign: element.align } : undefined
+  
   return (
-    <li {...attributes} style={style}>
+    <li {...attributes} style={style} className={getLanguageClass("en")}>
       {children}
     </li>
   )
