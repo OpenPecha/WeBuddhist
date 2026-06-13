c0117a88a037b1fe68b7f1f82a40b9c3639fc402
Tech-lo <taylorhent@gmail.com>
2026-06-13 21:13:18 +0530

    small fix

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -37,13 +37,13 @@ function ResizableHandle({
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
-        " bg-[#EDEDED] border-b border-[#e0e0e0]  focus-visible:ring-ring relative flex items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-2px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-2 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2",
+        " bg-[#EDEDED] border-b border-[#e0e0e0]  focus-visible:ring-ring relative flex items-center justify-center touch-none select-none after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-6 data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-full data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2",
         className,
       )}
       {...props}
     >
       {withHandle && (
-        <div className=" z-100  flex h-4 w-3 items-center justify-center rounded-xs">
+        <div className=" z-100  flex h-6 w-8 items-center justify-center rounded-xs">
           <LuGripHorizontal size={32} />
         </div>
       )}
